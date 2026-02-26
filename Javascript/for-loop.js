33cbcdb4926bcd80860178970f75e9fa901e2b75
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-02-26 18:11:53 +0530

    using for-in loop in pratice

---
+++ Javascript/for-loop.js
@@ -0,0 +1,15 @@
+let user = {
+
+        harry: 56,
+        akash: 67,
+        suraj: 78
+};
+
+for(a in user){
+
+        // a is the key 
+    //    user[a] is the value 
+
+      console.log(`${a} and his value is ${user[a]}`); 
+
+}
