a583b5df2df54159f7a852c948aec108cac83566
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-03-04 09:53:25 +0530

    we can name a single function two times but can call it by using it's first declaration name

---
+++ Javascript/named-function-expression.js
@@ -0,0 +1,20 @@
+
+
+// function expression
+
+var a = function (){
+
+
+}
+
+// named unction expression 
+
+var a = function abc() {
+
+    console.log("abc is a function");
+}
+
+// now call the function 
+
+a();
+abc(); // will get an error
