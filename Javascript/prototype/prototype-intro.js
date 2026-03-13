70c2faaf48c5bf12de179b789776ec6434d95705
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-03-13 11:14:14 +0530

    describes what prototype meansgit add prototype-intro.js

---
+++ Javascript/prototype/prototype-intro.js
@@ -0,0 +1,14 @@
+
+
+// what is prototype in js 
+// it's just another name --> for build-in functions of js 
+// for objects 
+
+let obj = {
+
+        name: "JS",
+        type: "scripting-language",
+        Isyes: true
+}
+
+console.log(obj.Isyes);
